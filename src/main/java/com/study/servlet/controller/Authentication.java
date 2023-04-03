b12634c540ee57e656ee0bf83ac353e7978361ed
dlgustn3768 <dlgustn3768@naver.com>
2023-04-03 17:51:41 +0900

    userRepository, userService, login.html 추가

--- src/main/java/com/study/servlet/controller/Authentication.java
+++ src/main/java/com/study/servlet/controller/Authentication.java
@@ -1,25 +1,20 @@
 package com.study.servlet.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.study.servlet.dto.RequestDto;
+import com.study.servlet.dto.ResponseDto;
 import com.study.servlet.entity.User;
+import com.study.servlet.service.UserService;
+import com.study.servlet.service.UserServiceImpl;
 
 /*
  * get 요청
@@ -39,91 +34,38 @@ import com.study.servlet.entity.User;
 @WebServlet("/auth")
 public class Authentication extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private Gson gson;
+	private UserService userService;
+	
+	public Authentication() {
+		userService = UserServiceImpl.getInstance();
+		gson = new Gson();
+	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setCharacterEncoding("UTF-8");
-		System.out.println("GET 요청");
-		
-		
-//		User user = new User();
 		String username = request.getParameter("username");
-		
-		List<User> userList = new ArrayList<>();
-		userList.add(new User("aaa", "1234", "a", "aaa@gmail.com"));
-		userList.add(new User("bbb", "2341", "b", "bbb@gmail.com"));
-		userList.add(new User("ccc", "3412", "c", "ccc@gmail.com"));
-		userList.add(new User("ddd", "4123", "d", "ddd@gmail.com"));
-		
-		User findUser = null;
-		
-		for (User user : userList) {
-			if(user.getUsername().equals(username)) {
-				findUser = user;
-				break;
-			}
-		}
-		Gson gson = new Gson();
 
-		String userJson = gson.toJson(findUser);
-		response.setContentType("application/json;charset=UTF-8");
-		response.getWriter().println(userJson);	
-		System.out.println(userJson);
-
-//		String password = request.getParameter("password");
-//		String name = request.getParameter("name");
-//		String email = request.getParameter("email");
-//		
-//		Map<String,String> userMap = new HashMap<>();
-//		userMap.put("username", username);
-//		userMap.put("password", password);
-//		userMap.put("name", name);
-//		userMap.put("email", email);
-//		
-//		System.out.println(username);
-//		String fineUsername = userMap.get("username");
-//		String finePassword = userMap.get("password");
-//		String fineName = userMap.get("name");
-//		String fineEmail = userMap.get("email");
-//		
-//		JsonObject responseData = new JsonObject();
-//		
-//		responseData.addProperty("username", fineUsername);
-//		responseData.addProperty("password", finePassword);
-//		responseData.addProperty("name", fineName);
-//		responseData.addProperty("email", fineEmail);
-		
-		
-		
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("aplication/json;Charset=UTF-8");
-		Gson gson = new Gson();
+		User user = RequestDto.<User> convertRequestBody(request, User.class);
+
+		boolean duplicatedFlag = userService.duplicatedUsername(user.getUsername());
 		
-		ServletInputStream inputStream = request.getInputStream();
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+		response.setContentType("application/json;charset=utf-8");
+		PrintWriter out = response.getWriter();
 		
-		String requestBody = bufferedReader.lines().collect(Collectors.joining());
-		
-		User user = gson.fromJson(requestBody, User.class);
-		
-		System.out.println(user);
-		
-		response.getWriter().println(requestBody);
-//		String requestJson = bufferedReader.lines().collect(Collectors.joining());
-//		
-//		List<Map<String, String>> requestMap = gson.fromJson(requestJson, List.class);
-//		
-//		PrintWriter out = response.getWriter();
-//		
-//		requestMap.forEach(obj -> {
-//			System.out.println("username: "+ obj.get("username") + " password: " + obj.get("password") + " name: " + obj.get("name") + " email: " + obj.get("email"));
-//			out.println("username: "+ obj.get("username") + " password: " + obj.get("password") + " name: " + obj.get("name") + " email: " + obj.get("email"));
-//		});
-		
+		if(duplicatedFlag) {			
+			// true == 중복, false == 가입가능
+			ResponseDto<Boolean> responseDto = 
+					new ResponseDto<Boolean>(400, "duplicated username", duplicatedFlag);
+			out.println(gson.toJson(responseDto));
+			return;
+		}
+		ResponseDto<Integer> responseDto = 
+				new ResponseDto<Integer>(201,"singip", userService.addUser(user));
+		out.println(gson.toJson(responseDto));
 	}
 
 }
